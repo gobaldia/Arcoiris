7fe82186017fe88319e1dd238339621aca6ee719
gobaldia <gobaldia@gmail.com>
2015-09-27 22:58:53 -0300

    Merge origin/master
    Conflicts:
    	src/PruebaGit.java

--- src/ArcoirisMainPackage/Partida.java
+++ src/ArcoirisMainPackage/Partida.java
@@ -1,5 +1,10 @@
 package ArcoirisMainPackage;
 
+import java.util.ArrayList;
+
 public class Partida {
+    Jugador jugadorA;
+    Jugador jugadorB;
+    ArrayList<Jugada> listaJugadas = new ArrayList<>();
     
 }
