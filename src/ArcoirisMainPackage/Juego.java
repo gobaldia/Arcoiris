ce61b6483f9f4cc18088c194cae832570502331a
gobaldia <gobaldia@gmail.com>
2015-09-28 12:47:21 -0300

    Merge origin/master
    Conflicts:
    	src/PruebaGit.java

--- src/ArcoirisMainPackage/Juego.java
+++ src/ArcoirisMainPackage/Juego.java
@@ -11,14 +11,20 @@ public class Juego {
         int filas = tablero.length;
         int cols = tablero[0].length;
         char[] letras = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'};
-        char[] numeros = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '1', '1', '1', '1'};
+        char[] numeros = {' ', '1', '2', '3', '4', '5', '6', '7', '8', '9', '1', '1', '1', '1'};
+        char[] numeros2 = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '0', '1', '2', '3'};
         
         for (int i = 0; i < filas; i++) {
             for (int j = 0; j < cols; j++) {
-                if (i == 0 && j > 0) {
-                    tablero[i][j] = numeros[j+1];
+                if (i == 0) {
+                    tablero[i][j] = numeros[j];
+                } else if (i == 1) {
+                    tablero[i][j] = numeros2[j];
+                } else if (j == 0) {
+                    tablero[i][j] = letras[i-2];
+                } else {
+                    tablero[i][j] = 'o';
                 }
-//                tablero[i][j] = 'o';
             }
         }
         
