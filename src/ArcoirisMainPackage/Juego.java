d45e71e8b9b31ac60ca805c1e833c8dd8b632631
gobaldia <gobaldia@gmail.com>
2015-09-28 00:29:58 -0300

    Merge origin/master
    Conflicts:
    	src/PruebaGit.java

--- src/ArcoirisMainPackage/Juego.java
+++ src/ArcoirisMainPackage/Juego.java
@@ -3,6 +3,28 @@ package ArcoirisMainPackage;
 public class Juego {
     
     public static void main(String[] args){
+        mostrarTablero();
+    }
+    
+    public static char[][] mostrarTablero(){
+        char[][] tablero = new char[15][14];
+        int filas = tablero.length;
+        int cols = tablero[0].length;
         
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < cols; j++) {
+                tablero[i][j] = 'o';
+            }
+        }
+        
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(tablero[i][j]);
+            }
+            System.out.println("");
+        }
+        
+        
+        return tablero;
     }
 }
