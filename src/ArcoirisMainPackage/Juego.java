aa20723b8157a314fe83be5dff545be5e9b7a001
santi_000 <santi_000@SantiPC>
2015-09-29 00:50:50 -0300

    Merge origin/master
    Conflicts:
    	src/Santiago.java
    	src/TestPintarCuadrados.java
    
    Se muestra el tablero correctamente (pero sin pintar) en la clase Juego

--- src/ArcoirisMainPackage/Juego.java
+++ src/ArcoirisMainPackage/Juego.java
@@ -58,4 +58,57 @@ public class Juego {
         }
         return tablero;
     }
+    
+    //Gabriel agregue este porque al hacer pull tenia mil conflictos.. luego lo vemos.. este muestra bien el tablero..
+    //me falta pintarlo q no tuve tiempo y no se porque no me anda la pintada >.<!
+    public static char[][] mostrarTablero2(){
+        char[][] tablero = new char[15][14];
+        int filas = tablero.length;
+        int cols = tablero[0].length;
+        char[] letras = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'};
+        char[] numeros = {' ', '1', '2', '3', '4', '5', '6', '7', '8', '9', '1', '1', '1', '1'};
+        char[] numeros2 = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '0', '1', '2', '3'};
+        
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (i == 0) {
+                    tablero[i][j] = numeros[j];
+                } else if (i == 1) {
+                    tablero[i][j] = numeros2[j];
+                } else if (j == 0) {
+                    tablero[i][j] = letras[i-2];
+                } else {
+                    tablero[i][j] = 'O';
+                }
+            }
+        }
+        
+        for (int i = 0; i < filas; i++) {            
+            if(i > 1){
+                System.out.println("  +-+-+-+-+-+-+-+-+-+-+-+-+-+");
+            } else if( i == 0 || i == 1){
+                System.out.print(" ");
+            }
+            
+            for (int j = 0; j < cols; j++) {
+                if(i > 1 && j > 0){
+                    System.out.print("|" + tablero[i][j]);
+                } else {
+                    System.out.print(tablero[i][j] + " ");
+                }
+            }
+            
+            if(i > 1){
+                System.out.print("|");
+            }
+            
+            if(i == tablero.length -1){
+                System.out.println();
+                System.out.println("  +-+-+-+-+-+-+-+-+-+-+-+-+-+");
+            }
+
+            System.out.println("");
+        }
+        return tablero;
+    }
 }
