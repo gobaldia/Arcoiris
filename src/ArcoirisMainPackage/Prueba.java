85c1a141d3631648e4845b09e843830e5fdedbc1
santi_000 <santi_000@SantiPC>
2015-10-04 22:00:46 -0200

    Merge origin/master
    Conflicts:
    	src/ArcoirisMainPackage/Juego.java
    
    Se agrego mostrarTablero con los colores

--- src/ArcoirisMainPackage/Prueba.java
+++ src/ArcoirisMainPackage/Prueba.java
@@ -7,11 +7,18 @@ import java.util.regex.Pattern;
 public class Prueba {
 
     public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_RESET = "\u001B[0m";
 
     public static void main(String[] args) {
         System.out.println("*-*-*-*-*- ARCOIRIS -*-*-*-*-*\n");
 
+        mostrarTablero2();
+        
         //Creo el Scanner para manejar los datos ingresados por el usuario.        
         Scanner input = new Scanner(System.in);
 
@@ -195,62 +202,123 @@ public class Prueba {
         return tablero;
     }
 
-    //Gabriel agregue este porque al hacer pull tenia mil conflictos.. luego lo vemos.. este muestra bien el tablero..
-    //me falta pintarlo q no tuve tiempo y no se porque no me anda la pintada >.<!
     public static char[][] mostrarTablero2() {
-        char[][] tablero = new char[15][14];
+        char[][] tablero = new char[13][13];
+        
+        for(int i=0; i < tablero.length; i++){
+            for(int j = 0; j < tablero[0].length; j++){
+                tablero[i][j] = 'O';
+            }
+        }
+        
         int filas = tablero.length;
         int cols = tablero[0].length;
         char[] letras = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'};
         char[] numeros = {' ', '1', '2', '3', '4', '5', '6', '7', '8', '9', '1', '1', '1', '1'};
         char[] numeros2 = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '0', '1', '2', '3'};
 
-        for (int i = 0; i < filas; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (i == 0) {
-                    tablero[i][j] = numeros[j];
-                } else if (i == 1) {
-                    tablero[i][j] = numeros2[j];
-                } else if (j == 0) {
-                    tablero[i][j] = letras[i - 2];
-                } else {
-                    tablero[i][j] = 'O';
-                }
-            }
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print(" " + numeros[i]);
         }
-
+        System.out.println();
+        for (int i = 0; i < numeros2.length; i++) {
+            System.out.print(" " + numeros2[i]);
+        }
+        System.out.println();
+        
+        
         for (int i = 0; i < filas; i++) {
-            if (i > 1) {
-                System.out.println("  +-+-+-+-+-+-+-+-+-+-+-+-+-+");
-            } else if (i == 0 || i == 1) {
-                System.out.print(" ");
-            }
+            System.out.println("  +-+-+-+-+-+-+-+-+-+-+-+-+-+");
+            System.out.print(letras[i] + " ");
 
             for (int j = 0; j < cols; j++) {
-                if (i > 1 && j > 0) {
+                if (medirDistanciaMarco(i, j, 1)) {
+                    System.out.print("|" + ANSI_RED + tablero[i][j] + ANSI_RESET);
+                } else if (medirDistanciaMarco(i, j, 2)) {
+                    System.out.print("|" + ANSI_PURPLE + tablero[i][j] + ANSI_RESET);
+                } else if (medirDistanciaMarco(i, j, 3)) {
+                    System.out.print("|" + ANSI_YELLOW + tablero[i][j] + ANSI_RESET);
+                } else if (medirDistanciaMarco(i, j, 4)) {
+                    System.out.print("|" + ANSI_GREEN + tablero[i][j] + ANSI_RESET);
+                } else if (medirDistanciaMarco(i, j, 5)) {
+                    System.out.print("|" + ANSI_CYAN + tablero[i][j] + ANSI_RESET);
+                } else if (medirDistanciaMarco(i, j, 6)) {
+                    System.out.print("|" + ANSI_BLUE + tablero[i][j] + ANSI_RESET);
+                } else {
                     System.out.print("|" + tablero[i][j]);
-                } else {
-                    System.out.print(tablero[i][j] + " ");
-                }
+                }                
             }
-
-            if (i > 1) {
-                System.out.print("|");
-            }
-
-            if (i == tablero.length - 1) {
-                System.out.println();
-                System.out.println("  +-+-+-+-+-+-+-+-+-+-+-+-+-+");
-            }
-
-            System.out.println("");
+            
+            System.out.print("|");
+            System.out.println();
         }
-        return tablero;
+        System.out.println("  +-+-+-+-+-+-+-+-+-+-+-+-+-+");
+        
+        return tablero;      
     }
 
-    /**
-     * **********************************************
-     * //**********************************************
-     * //**********************************************
-     */
+    public static boolean medirDistanciaMarco(int fila, int columna, int marcoAPintar) {
+        boolean bandera = false;
+
+        switch (marcoAPintar) {
+            case 1:
+                if ((fila == 0 && (columna >= 0 && columna <= 12))
+                        || (columna == 0 && (fila >= 0 && fila <= 12))) {
+                    bandera = true;
+                } else if ((fila == 12 && (columna >= 0 && columna <= 12))
+                        || (columna == 12 && (fila >= 0 && fila <= 12))) {
+                    bandera = true;
+                }
+                break;
+            case 2:
+                if ((fila == 1 && (columna >= 1 && columna <= 11))
+                        || (columna == 1 && (fila >= 1 && fila <= 11))) {
+                    bandera = true;
+                } else if ((fila == 11 && (columna >= 1 && columna <= 11))
+                        || (columna == 11 && (fila >= 1 && fila <= 11))) {
+                    bandera = true;
+                }
+                break;
+            case 3:
+                if ((fila == 2 && (columna >= 2 && columna <= 10))
+                        || (columna == 2 && (fila >= 2 && fila <= 10))) {
+                    bandera = true;
+                } else if ((fila == 10 && (columna >= 2 && columna <= 10))
+                        || (columna == 10 && (fila >= 2 && fila <= 10))) {
+                    bandera = true;
+                }
+                break;
+            case 4:
+                if ((fila == 3 && (columna >= 3 && columna <= 9))
+                        || (columna == 3 && (fila >= 3 && fila <= 9))) {
+                    bandera = true;
+                } else if ((fila == 9 && (columna >= 3 && columna <= 9))
+                        || (columna == 9 && (fila >= 3 && fila <= 9))) {
+                    bandera = true;
+                }
+                break;
+            case 5:
+                if ((fila == 4 && (columna >= 4 && columna <= 8))
+                        || (columna == 4 && (fila >= 4 && fila <= 8))) {
+                    bandera = true;
+                } else if ((fila == 8 && (columna >= 4 && columna <= 8))
+                        || (columna == 8 && (fila >= 4 && fila <= 8))) {
+                    bandera = true;
+                }
+                break;
+            case 6:
+                if ((fila == 5 && (columna >= 5 && columna <= 7))
+                        || (columna == 5 && (fila >= 5 && fila <= 7))) {
+                    bandera = true;
+                } else if ((fila == 7 && (columna >= 5 && columna <= 7))
+                        || (columna == 7 && (fila >= 5 && fila <= 7))) {
+                    bandera = true;
+                }
+                break;
+            default:
+                break;
+        }
+
+        return bandera;
+    }
 }
