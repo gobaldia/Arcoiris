d2f3d5cc121363eeadad34385214ebb54213aec4
santi_000 <santi_000@SantiPC>
2015-11-10 02:04:49 -0200

    Timer y progressBar

--- Arcoiris/src/Interfaz/VentanaJugar.java
+++ Arcoiris/src/Interfaz/VentanaJugar.java
@@ -6,6 +6,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.Toolkit;
@@ -19,6 +20,7 @@ import java.util.logging.Logger;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
+import javax.swing.Timer;
 
 public class VentanaJugar extends javax.swing.JFrame {
 
@@ -29,6 +31,7 @@ public class VentanaJugar extends javax.swing.JFrame {
     private JButton[][] botones;
     private JButton[] btnColumnas;
     private JButton[] btnFilas;
+    private Timer timer;
 
     public VentanaJugar(Juego unJuego, VentanaPrincipal ventanaPrincipal) {
         initComponents();
@@ -68,10 +71,20 @@ public class VentanaJugar extends javax.swing.JFrame {
             jlblJugadorA.setText(this.getPartidaActual().getJugadorA().getAlias());
             jlblJugadorB.setText(this.getPartidaActual().getJugadorB().getAlias());
 
+            if (this.getPartidaActual().getTimer()) {
+                jlblFin.setVisible(true);
+                jlblInicio.setVisible(true);
+                jProgressBarTimer.setVisible(true);
+                ejecutarTimer();
+                timer.start();
+            } else {
+                jProgressBarTimer.setVisible(false);
+                jlblFin.setVisible(false);
+                jlblInicio.setVisible(false);
+            }
+
             generarIndicesFilaColumna();
             mostrarTablero(this.getPartidaActual().getListaDeTableros().get(0).getMatriz());
-            
-//            generarGrilla();
 
         } else {
             JOptionPane.showMessageDialog(this, "No existen partidas configuradas.", "Error", JOptionPane.CANCEL_OPTION);
@@ -121,10 +134,19 @@ public class VentanaJugar extends javax.swing.JFrame {
         jPanelFilas = new javax.swing.JPanel();
         jPanelDetalle = new javax.swing.JPanel();
         jlblJugadorA = new javax.swing.JLabel();
-        jlblJugadorB = new javax.swing.JLabel();
         jlblVS = new javax.swing.JLabel();
+        jlblJugadorB = new javax.swing.JLabel();
+        jlblTitulo = new javax.swing.JLabel();
+        jProgressBarTimer = new javax.swing.JProgressBar();
+        jlblInicio = new javax.swing.JLabel();
+        jlblFin = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        jlblTurnoDe = new javax.swing.JLabel();
+        jlblDetalleJugadasTitulo = new javax.swing.JLabel();
+        jlblDetalleJugadas = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setPreferredSize(new java.awt.Dimension(1100, 600));
         setResizable(false);
 
         jPanelJugar.setPreferredSize(new java.awt.Dimension(600, 500));
@@ -140,17 +162,18 @@ public class VentanaJugar extends javax.swing.JFrame {
             .addGap(0, 501, Short.MAX_VALUE)
         );
 
+        jPanelColumnas.setMaximumSize(new java.awt.Dimension(400000, 400000));
         jPanelColumnas.setPreferredSize(new java.awt.Dimension(600, 40));
 
         javax.swing.GroupLayout jPanelColumnasLayout = new javax.swing.GroupLayout(jPanelColumnas);
         jPanelColumnas.setLayout(jPanelColumnasLayout);
         jPanelColumnasLayout.setHorizontalGroup(
             jPanelColumnasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 600, Short.MAX_VALUE)
+            .addGap(0, 0, Short.MAX_VALUE)
         );
         jPanelColumnasLayout.setVerticalGroup(
             jPanelColumnasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 40, Short.MAX_VALUE)
+            .addGap(0, 45, Short.MAX_VALUE)
         );
 
         jPanelFilas.setPreferredSize(new java.awt.Dimension(40, 500));
@@ -159,41 +182,108 @@ public class VentanaJugar extends javax.swing.JFrame {
         jPanelFilas.setLayout(jPanelFilasLayout);
         jPanelFilasLayout.setHorizontalGroup(
             jPanelFilasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 40, Short.MAX_VALUE)
+            .addGap(0, 51, Short.MAX_VALUE)
         );
         jPanelFilasLayout.setVerticalGroup(
             jPanelFilasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 500, Short.MAX_VALUE)
         );
 
-        jlblJugadorA.setText("_______");
-
-        jlblJugadorB.setText("_______");
+        jlblJugadorA.setText("__________");
 
         jlblVS.setFont(new java.awt.Font("Tahoma", 1, 13)); // NOI18N
         jlblVS.setText("vs");
+
+        jlblJugadorB.setText("__________");
+
+        jlblTitulo.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jlblTitulo.setText("Partida Actual");
+
+        jProgressBarTimer.setToolTipText("..Tic..Tac..");
+
+        jlblInicio.setFont(new java.awt.Font("Tahoma", 2, 13)); // NOI18N
+        jlblInicio.setText("INICIO");
+
+        jlblFin.setFont(new java.awt.Font("Tahoma", 2, 13)); // NOI18N
+        jlblFin.setText("FIN");
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 13)); // NOI18N
+        jLabel1.setText("Turno de:");
+
+        jlblTurnoDe.setText("__________");
+
+        jlblDetalleJugadasTitulo.setFont(new java.awt.Font("Tahoma", 1, 13)); // NOI18N
+        jlblDetalleJugadasTitulo.setText("< Detalle de Jugadas >");
+
+        jlblDetalleJugadas.setText("__________________");
 
         javax.swing.GroupLayout jPanelDetalleLayout = new javax.swing.GroupLayout(jPanelDetalle);
         jPanelDetalle.setLayout(jPanelDetalleLayout);
         jPanelDetalleLayout.setHorizontalGroup(
             jPanelDetalleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanelDetalleLayout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jlblJugadorA)
-                .addGap(30, 30, 30)
+                .addGap(27, 27, 27)
+                .addComponent(jlblJugadorA, javax.swing.GroupLayout.PREFERRED_SIZE, 83, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jlblVS)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 31, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 33, Short.MAX_VALUE)
                 .addComponent(jlblJugadorB)
-                .addGap(23, 23, 23))
+                .addGap(31, 31, 31))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanelDetalleLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jProgressBarTimer, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+            .addGroup(jPanelDetalleLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jlblInicio)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jlblFin, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+            .addGroup(jPanelDetalleLayout.createSequentialGroup()
+                .addGroup(jPanelDetalleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanelDetalleLayout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jlblTurnoDe, javax.swing.GroupLayout.PREFERRED_SIZE, 143, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanelDetalleLayout.createSequentialGroup()
+                        .addGap(50, 50, 50)
+                        .addComponent(jlblDetalleJugadasTitulo)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanelDetalleLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jlblDetalleJugadas, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanelDetalleLayout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jlblTitulo)
+                .addGap(87, 87, 87))
         );
         jPanelDetalleLayout.setVerticalGroup(
             jPanelDetalleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanelDetalleLayout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(jPanelDetalleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jlblJugadorA)
+                .addComponent(jlblTitulo)
+                .addGap(30, 30, 30)
+                .addGroup(jPanelDetalleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jlblJugadorB)
-                    .addComponent(jlblVS))
+                    .addGroup(jPanelDetalleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jlblJugadorA)
+                        .addComponent(jlblVS)))
+                .addGap(22, 22, 22)
+                .addGroup(jPanelDetalleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jlblInicio)
+                    .addComponent(jlblFin))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jProgressBarTimer, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(jPanelDetalleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(jlblTurnoDe))
+                .addGap(18, 18, 18)
+                .addComponent(jlblDetalleJugadasTitulo)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jlblDetalleJugadas)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
@@ -202,57 +292,74 @@ public class VentanaJugar extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(9, 9, 9)
-                .addComponent(jPanelFilas, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jPanelColumnas, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jPanelJugar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addComponent(jPanelFilas, javax.swing.GroupLayout.PREFERRED_SIZE, 51, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jPanelColumnas, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jPanelJugar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jPanelDetalle, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
+                .addComponent(jPanelDetalle, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jPanelDetalle, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jPanelColumnas, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 13, Short.MAX_VALUE)
+                        .addComponent(jPanelColumnas, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jPanelJugar, javax.swing.GroupLayout.PREFERRED_SIZE, 501, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jPanelFilas, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addContainerGap(31, Short.MAX_VALUE))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jPanelFilas, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jPanelJugar, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 501, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(1, 1, 1))
+                    .addComponent(jPanelDetalle, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void ejecutarTimer() {
+        this.timer = new Timer(1000, new ActionListener() {
+            private int counter = 1;
+
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                int val = jProgressBarTimer.getValue();
+                if (val >= 100) {
+                    timer.stop();
+                    return;
+                }
+                jProgressBarTimer.setValue(++val);
+            }
+        });
+    }
+
     private void generarIndicesFilaColumna() {
-        jPanelColumnas.setLayout(new FlowLayout(13));
+        jPanelColumnas.setLayout(new GridLayout(1, 13));
         btnColumnas = new JButton[13];
 
         for (int i = 0; i < btnColumnas.length; i++) {
             JButton nuevoBoton = new JButton();
-            nuevoBoton.setPreferredSize(new Dimension(40, 30));
-            nuevoBoton.setFont(new Font("Arial", Font.BOLD, 10));
+            nuevoBoton.setPreferredSize(new Dimension(30, 30));
+            nuevoBoton.setFont(new Font("Arial", Font.BOLD, 15));
             nuevoBoton.setText(i + 1 + "");
             nuevoBoton.setBackground(Color.GRAY);
+
             jPanelColumnas.add(nuevoBoton);
             btnColumnas[i] = nuevoBoton;
         }
 
-        jPanelFilas.setLayout(new FlowLayout(13));
+        jPanelFilas.setLayout(new GridLayout(13, 1));
         btnFilas = new JButton[13];
+        char[] letras = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'};
 
         for (int i = 0; i < btnFilas.length; i++) {
             JButton nuevoBoton = new JButton();
-            nuevoBoton.setPreferredSize(new Dimension(40, 30));
-            nuevoBoton.setFont(new Font("Arial", Font.BOLD, 10));
+            nuevoBoton.setPreferredSize(new Dimension(30, 30));
+            nuevoBoton.setFont(new Font("Arial", Font.BOLD, 15));
             nuevoBoton.setBackground(Color.GRAY);
-            nuevoBoton.setText(i + 1 + "");
+            nuevoBoton.setText(letras[i] + "");
             //nuevoBoton.setEnabled(false);
             jPanelFilas.add(nuevoBoton);
             btnFilas[i] = nuevoBoton;
@@ -260,12 +367,20 @@ public class VentanaJugar extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanelColumnas;
     private javax.swing.JPanel jPanelDetalle;
     private javax.swing.JPanel jPanelFilas;
     private javax.swing.JPanel jPanelJugar;
+    private javax.swing.JProgressBar jProgressBarTimer;
+    private javax.swing.JLabel jlblDetalleJugadas;
+    private javax.swing.JLabel jlblDetalleJugadasTitulo;
+    private javax.swing.JLabel jlblFin;
+    private javax.swing.JLabel jlblInicio;
     private javax.swing.JLabel jlblJugadorA;
     private javax.swing.JLabel jlblJugadorB;
+    private javax.swing.JLabel jlblTitulo;
+    private javax.swing.JLabel jlblTurnoDe;
     private javax.swing.JLabel jlblVS;
     // End of variables declaration//GEN-END:variables
 
@@ -288,7 +403,7 @@ public class VentanaJugar extends javax.swing.JFrame {
                 for (int j = 0; j < 13; j++) {
                     JButton jButton = new JButton();
                     jButton.addActionListener(new ListenerBoton(i, j));
-                    jButton.setPreferredSize(new Dimension(20, 20));
+                    jButton.setPreferredSize(new Dimension(30, 30));
                     jButton.setFont(new Font("Arial", Font.PLAIN, 10));
                     jPanelJugar.add(jButton);
                     botones[i][j] = jButton;
@@ -351,7 +466,7 @@ public class VentanaJugar extends javax.swing.JFrame {
         }
     }
 
-        private boolean medirDistanciaMarco(int fila, int columna, int marcoAPintar) {
+    private boolean medirDistanciaMarco(int fila, int columna, int marcoAPintar) {
         boolean bandera = false;
 
         switch (marcoAPintar) {
@@ -415,7 +530,7 @@ public class VentanaJugar extends javax.swing.JFrame {
 
         return bandera;
     }
-    
+
     private class ListenerBoton implements ActionListener {
 
         private int x;
