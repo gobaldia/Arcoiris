9dda820ed304f3ba7571f42da9573b1eb9d2bdf5
santi_000 <santi_000@SantiPC>
2015-11-22 18:14:39 -0200

    Correccion al terminar partida por cantidad de movimientos

--- Arcoiris/src/Interfaz/VentanaRanking.java
+++ Arcoiris/src/Interfaz/VentanaRanking.java
@@ -1,10 +1,8 @@
 package Interfaz;
 
 import ArcoirisMainPackage.Juego;
-import ArcoirisMainPackage.Partida;
 import java.awt.Dimension;
 import java.awt.Toolkit;
-import javax.swing.JLabel;
 
 public class VentanaRanking extends javax.swing.JFrame {
 
@@ -28,7 +26,23 @@ public class VentanaRanking extends javax.swing.JFrame {
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
         this.setLocation(dim.width / 3 - this.getSize().width / 3, dim.height / 3 - this.getSize().height / 3);
 
+        //getModelo().obtenerRanking();
 
+        /*
+        if (unJuego.getListaDeJugadores().isEmpty()) {
+            System.out.println("\nNo existen jugadores.");
+        } else {
+            for (int i = 0; i < unJuego.getListaDeJugadores().size(); i++) {
+                Jugador j = unJuego.getListaDeJugadores().get(i);
+                System.out.println(j.toString() + " || " + "Ganadas: " + j.getGanadas()
+                        + " | Empatadas: " + j.getEmpates()
+                        + " | Perdidas: " + j.getPerdidas());
+            }
+        }
+        */
+        
+        
+        
     }
 
     // <editor-fold defaultstate="collapsed" desc="Getters y Setters">
@@ -55,11 +69,21 @@ public class VentanaRanking extends javax.swing.JFrame {
 
         jPanelRanking = new javax.swing.JPanel();
         jlblTituloRanking = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jListRanking = new javax.swing.JList();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setResizable(false);
 
         jlblTituloRanking.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
         jlblTituloRanking.setText("Ranking");
+
+        jListRanking.setModel(new javax.swing.AbstractListModel() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public Object getElementAt(int i) { return strings[i]; }
+        });
+        jScrollPane1.setViewportView(jListRanking);
 
         javax.swing.GroupLayout jPanelRankingLayout = new javax.swing.GroupLayout(jPanelRanking);
         jPanelRanking.setLayout(jPanelRankingLayout);
@@ -67,15 +91,19 @@ public class VentanaRanking extends javax.swing.JFrame {
             jPanelRankingLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanelRankingLayout.createSequentialGroup()
                 .addGap(35, 35, 35)
-                .addComponent(jlblTituloRanking, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(298, Short.MAX_VALUE))
+                .addGroup(jPanelRankingLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jlblTituloRanking, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 402, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(28, Short.MAX_VALUE))
         );
         jPanelRankingLayout.setVerticalGroup(
             jPanelRankingLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanelRankingLayout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jlblTituloRanking, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(389, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 343, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(33, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -93,7 +121,9 @@ public class VentanaRanking extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JList jListRanking;
     private javax.swing.JPanel jPanelRanking;
+    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel jlblTituloRanking;
     // End of variables declaration//GEN-END:variables
 }
